9a81f289237c7d4e1192128770104bee062a7e86
anguyenliveops <anguyen@liveops.com>
2017-09-11 11:58:39 -0700

    Tests file

---
+++ yelp/test.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+NODE_ENV=yelp ../node_modules/.bin/wdio wdio.conf.js --spec ./test/specs/yelp.js
+mv reports/results-0-0.chrome.xml reports/pizza-0-0.chrome.xml
+NODE_ENV=yelp ../node_modules/.bin/wdio wdio.conf.js --search2=chinese --spec ./test/specs/yelp.js
+mv reports/results-0-0.chrome.xml reports/chinese-0-0.chrome.xml
+NODE_ENV=yelp ../node_modules/.bin/wdio wdio.conf.js --search1=Home --search2=alarm --spec ./test/specs/yelp.js
+mv reports/results-0-0.chrome.xml reports/homeAlarm-0-0.chrome.xml
+set +e
+# ../node_modules/.bin/wdio wdio.conf.js --search1=Carasdf --search2=alarm --spec ./test/specs/yelp.js
+set -e

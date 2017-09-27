62b3bfa702fcc3ef84bc2531bcd742046f0db39d
anguyenliveops <anguyen@liveops.com>
2017-09-27 00:33:44 -0700

    disable headless

--- yelp/wdio.conf.js
+++ yelp/wdio.conf.js
@@ -52,7 +52,7 @@ exports.config = {
         // 5 instances get started at a time.
         maxInstances: 5,
         //
-        chromeOptions: {
+        chromeadfOptions: {
             args: ['--headless', '--disable-gpu', '--window-size=1280,800'],
             binary: '/Applications/Google\ Chrome.app/Contents/MacOS/Google\ Chrome'
         },
